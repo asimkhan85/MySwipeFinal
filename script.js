0d6b1cc6140fad6b9c4a722c490f93f3b9be6fd7
asimkhan85 <asimkhan83437@gmail.com>
2025-11-30 11:47:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-let totalImages = 6; // कितनी photos हैं
-let current = 1;
+let images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+let index = 0;
 
-const img = document.getElementById("productImage");
+let img = document.getElementById("mainImage");
 
-window.addEventListener("touchmove", (e) => {
-    let x = e.touches[0].clientX;
+let startX = 0;
+let endX = 0;
 
-    if (x % 20 === 0) {
-        current++;
-        if (current > totalImages) current = 1;
-        img.src = current + ".png";
+img.addEventListener("touchstart", (e) => {
+    startX = e.touches[0].clientX;
+});
+
+img.addEventListener("touchend", (e) => {
+    endX = e.changedTouches[0].clientX;
+
+    if (startX - endX > 50) {
+        nextImage();
+    } else if (endX - startX > 50) {
+        previousImage();
     }
 });
+
+function nextImage() {
+    index = (index + 1) % images.length;
+    img.src = images[index];
+}
+
+function previousImage() {
+    index = (index - 1 + images.length) % images.length;
+    img.src = images[index];
+}
